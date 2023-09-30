1e979c87505c9e9742fd945ce22ff10490ddeabd
serafal <serathim1357@gmail.com>
2023-09-30 16:06:44 +0300

    Примерная структура проекта V1

---
+++ src/CRON-APP/cron-app.jsx
@@ -0,0 +1,7 @@
+function CRON_APP() {
+   return (
+    <div>123</div>
+   ) 
+}
+
+export default CRON_APP;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import CRON_APP from './CRON-APP/cron-app';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <CRON_APP/>
   </React.StrictMode>
 );
